d7823d079e6d33cb4a597ff0de58a788a97e196c
Muhammed Misir <muhammed.misir@googlemail.com>
2021-07-02 02:41:05 +0200

    added formcontrol

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Todo } from 'src/app/models/todo';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  todos: Todo[] = [];
 
-  constructor() { }
+  todoInput = new FormControl('');
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  add() {
+    if (!this.todoInput.value) return;
+
+    this.todos.push({
+      text: this.todoInput.value,
+      createdAt: new Date(),
+    });
+
+    this.todoInput.reset();
   }
-
 }
